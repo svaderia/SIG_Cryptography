eb976e4a1572b4af062f28368f48281fef0998ae
Apurv Bajaj <apurvabajaj007@gmail.com>
2017-06-19 16:59:16 +0530

    Delete p2.py

--- p2.py
+++
@@ -1,7 +0,0 @@
-from binascii import hexlify,unhexlify,a2b_hex,b2a_hex 
-h1=raw_input("Hex String 1>")
-h2=raw_input("Hex String 2>")
-s3=str(hex(int(h1,16)^int(h2,16)))
-s=s3[2:len(s3)-1]
-print s
-
