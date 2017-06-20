fd47f15c132089ed59b23ce2c33bf33c7f882d4c
jbnerd <abhivjoshi.aj@gmail.com>
2017-06-20 19:35:27 +0530

    Set1 Problem1 Cryptopals jbnerd added.

---
+++ Cryptopal/Set_1/Problem_1/JBNerd/convert.py
@@ -0,0 +1,26 @@
+import base64
+import binascii
+
+def hextobase64(s):
+	raw = binascii.unhexlify(s)
+	return base64.b64encode(raw).decode('ascii')
+
+def base64tohex(s):
+	raw = base64.b64decode(s)
+	return binascii.hexlify(raw)
+
+def hextoraw(s):
+	return binascii.unhexlify(s)
+
+def main():
+	hex = input()
+	b64 = hextobase64(hex)
+	b16 = base64tohex(b64)
+	raw = hextoraw(hex)
+
+	print(b16)
+	print(b64)
+	print(raw)
+
+if __name__ == "__main__":
+	main()
