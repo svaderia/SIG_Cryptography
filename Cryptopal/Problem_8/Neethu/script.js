96d96c7c6f5db5e7e4c4265c827a0810e2c227f1
Roboneet <neethumariyajoy@gmail.com>
2017-07-20 05:45:45 +0530

    detecting aes ecb

---
+++ Cryptopal/Problem_8/Neethu/script.js
@@ -0,0 +1,121 @@
+var fs = require('fs');
+var split_stream = require('split');
+var Transform = require('stream').Transform;
+var util = require('util');
+
+util.inherits(ProcessLine, Transform);
+util.inherits(EvaluateResult, Transform);
+
+var file = fs.createReadStream('./input.txt');
+
+var data = []
+
+file.on('error', function(){
+	console.log('err');
+})
+// console.log(Transform.super_.super_.super_.super_.init())
+console.log("It'll take a few mins ...")
+file.pipe(split_stream()).pipe(new ProcessLine).pipe(new EvaluateResult).on('finish', function(){
+	var sorted = data.sort(function(a,b){
+		return b[1] - a[1];
+	})
+	var str = '';
+	str += 'The line encoded with ecb is : line no '+ sorted[0][0];
+	str += '\ntop 10 finalist lines :\n[<line_no>, <eligibility>]\n';
+	str += sorted.slice(0,10).join('\n');
+	fs.writeFile('./output.txt', str, function(){
+		console.log(str);
+	})
+})
+function ProcessLine(){
+	Transform.call(this, {'objectMode':true})
+	this.line = 0;
+}
+function EvaluateResult(){
+	Transform.call(this, {'objectMode':true})
+}
+
+ProcessLine.prototype._transform = function(line, encoding, processed){
+	var blocks = line.split_string(32);
+	var distances = [];
+	for(var i=0; i<blocks.length-1; i++){
+		for(var j=i+1; j< blocks.length; j++){
+			var len = 3;
+			var array =[];
+			for(; len<33;len++){
+				for(var k = 0; k< 32 - len; k++){
+					array.push(hamming_distance(blocks[i].slice(i, i+len), blocks[j].slice(i, i+len)))
+				}
+
+			}
+			distances.push(array)
+		}
+
+	}
+	this.line++;
+	this.push({line: this.line, info: distances});
+	
+	processed();
+}
+
+
+EvaluateResult.prototype._transform = function(object, encoding, processed){
+	var count_zeros = 0;
+	object.info.forEach(function(row){
+		row.forEach(function(ele){
+			if(ele==0)count_zeros++;
+		})
+	})
+	data.push([object.line,count_zeros]);
+	processed()
+}
+
+
+/*= split string to blocks of specified length */
+String.prototype.split_string = function(size){
+    var regex = new RegExp("(.|[\r\n]){1,"+size+"}",'g');
+    return this.match(regex);
+}
+
+/*= pad zero in the front*/
+function zero_pad(string, length){
+    if(string.length>=length){
+        return string;
+    }
+    return zero_pad('0'+string, length)
+}
+
+// function hex_binary(hex_string){
+// 	var bin_length = hex_string.length*4;
+// 	var bin = parseInt(hex_string, 16).toString(2);
+// 	if(bin_length> bin.length){
+// 		return zero_pad(bin, bin_length)
+// 	}
+// 	return bin;
+// }
+
+function hamming_distance(hex1, hex2){
+	var xor = xor_hex(hex1, hex2)
+	
+	count = 0;
+	xor.split('').forEach(function(ele){
+		if(parseInt(ele))count++;
+	})
+	return count;
+}
+
+function xor_hex(hex1, hex2){
+	if(hex1.length != hex2.length){
+		console.log('strings should be of the same length')
+		return
+	}
+	var xor_bin = ''
+	hex1.split('').forEach(function(ele, ind){
+		xor_bin += zero_pad((parseInt(ele, 16)^parseInt(hex2[ind])).toString(2),4)
+	})
+	return xor_bin;
+
+}
+
+// console.log(hamming_distance('f0','f1'))
+
